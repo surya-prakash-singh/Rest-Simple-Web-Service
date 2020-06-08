7a7dae98ae1aeb3183a48db3c858481f935ecc51
surya.singh <suryapsingh@L-156179489-SURYAPRAKASHSINGH.local>
2020-06-08 16:08:01 +0100

    Add more documentation Swagger

--- src/main/java/com/webservices/restful/social/SwaggerCoonfig.java
+++ src/main/java/com/webservices/restful/social/SwaggerCoonfig.java
@@ -2,17 +2,31 @@ package com.webservices.restful.social;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 @Configuration      //Declare its a config file
 @EnableSwagger2     //Enabling Swagger2
 public class SwaggerCoonfig {
 
+    private static final ApiInfo DEFAULT_API_INFO = new ApiInfo(
+            "Social Api","Social Network Api's","1.0","urn:tos","Surya Prakash","MIT","mitUrl"
+    );
+    private static final Set<String> DEFAULT_PRODUCES_CONSUMES = new HashSet<String>(Arrays.asList("application/json","application/xml"));
+
     @Bean
     public Docket api(){
-        return new Docket(DocumentationType.SWAGGER_2);
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(DEFAULT_API_INFO)
+                .produces(DEFAULT_PRODUCES_CONSUMES)
+                .consumes(DEFAULT_PRODUCES_CONSUMES);
     }
 
     //Bean Docket
--- src/main/java/com/webservices/restful/social/User.java
+++ src/main/java/com/webservices/restful/social/User.java
@@ -1,16 +1,22 @@
 package com.webservices.restful.social;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 import java.util.List;
 
+@ApiModel(description = "All User Description")
 public class User {
 
     private Integer id;
     @Size(min=2, message = "Name should have atleast 2 characters")
+    @ApiModelProperty(notes="Name should be atleast 2 characters")
     private String name;
     @Past
+    @ApiModelProperty(notes = "Date of Birth should be in past")
     private Date dateofbirth;
     private List<Post> posts;
 
