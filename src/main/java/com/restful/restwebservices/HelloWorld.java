3adca49f45bab0cc22f95b3478dcdf8a21521dd4
surya.singh <suryapsingh@L-156179489-SURYAPRAKASHSINGH.local>
2020-06-08 18:46:20 +0100

    Internationalization

--- src/main/java/com/restful/restwebservices/HelloWorld.java
+++ src/main/java/com/restful/restwebservices/HelloWorld.java
@@ -3,6 +3,7 @@ package com.restful.restwebservices;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Locale;
@@ -31,4 +32,9 @@ public class HelloWorld {
     public String HelloWorldInternationalized(@RequestHeader(name= "Accept-Language",required = false) Locale locale){
         return messageSource.getMessage("good.morning.message",null,locale);
     }
+
+//    @GetMapping(path="/hello-world-internationalized")
+//    public String HelloWorldInternationalized(){
+//        return messageSource.getMessage("good.morning.message",null, LocaleContextHolder.getLocale());
+//    }
 }
